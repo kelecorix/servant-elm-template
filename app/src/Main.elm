c44bbdcaf9473662d4e69d3a1f404520cb70f30b
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-12 11:38:57 +0200

    update app structure

---
+++ app/src/Main.elm
@@ -0,0 +1,17 @@
+port module Main exposing (main)
+
+import Navigation as Nav
+import Types exposing (..)
+import State exposing (..)
+import View exposing (..)
+
+--------------------------------------------------------------------------------
+
+main : Program Flags Model Msg
+main =
+    Nav.programWithFlags UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ app/src/Types.elm
@@ -0,0 +1,54 @@
+module Types exposing (..)
+
+import Material
+import Navigation as Nav
+import RemoteData exposing (WebData)
+import Client.Login.Login as Login exposing (Model)
+import Client.Pages.Home.Home as HomePage exposing (Msg(..))
+
+
+--------------------------------------------------------------------------------
+
+
+type alias Flags =
+    { nodeEnv : String
+    , apiBaseUrl : String
+    }
+
+
+type alias AppConfig =
+    { apiBaseUrl : String }
+
+
+type alias Model =
+    { appConfig : AppConfig
+    , history : List Nav.Location
+    , currentViewModel : ViewModel
+    }
+
+
+type ViewModel
+    = Login Login.Model
+    | HomePage HomePage.Model
+
+
+type ViewState
+    = LoginView
+    | HomePageView
+
+
+type ChildPageMsgs
+    = HomePageMsg HomePage.Msg
+
+
+type Msg
+    = UrlChange Nav.Location
+    | Mdl (Material.Msg Msg)
+    | LoginMsg Login.Msg Login.Model
+    | ChildPageMsg ChildPageMsgs
+    | ChangeView ViewState (Maybe String)
+    | ReceiveAuthentication (RemoteData.WebData String)
+
+
+type alias Url =
+    String
